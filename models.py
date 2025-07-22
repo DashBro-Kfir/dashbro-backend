62cdf2425b5a0490277c1967d4789022e5c030d1
lior <liorazar456@gmail.com>
2025-07-22 10:56:30 +0300

    auth without logger

---
+++ models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+
+class SignupRequest(BaseModel):
+    username: str
+    firstname: str
+    surname: str
+    password: str
+    email: EmailStr
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    firstname: str
+    surname: str
+    email: EmailStr
